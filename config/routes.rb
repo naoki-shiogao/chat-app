f098ee8d6d62bc93d2867e416a286d4149fd8ee4
naoki-shiogao <138964908+naoki-shiogao@users.noreply.github.com>
2023-07-22 17:44:10 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users, only: [:edit, :update] 
   resources :messages
+  resources :rooms, only: [:new, :create]
   devise_for :users
   root to: "messages#index"
 end

e25a59bb42fb4145a633aab1ca3ea8e210148ccc
naoki-shiogao <138964908+naoki-shiogao@users.noreply.github.com>
2023-07-24 16:46:15 +0900

    プルダウンの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :users, only: [:edit, :update] 
-  resources :messages
   resources :rooms, only: [:new, :create]
   devise_for :users
   root to: "messages#index"

db85dde11cae385753d693e1251915f1de73a2ad
naoki-shiogao <138964908+naoki-shiogao@users.noreply.github.com>
2023-07-25 16:40:50 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :users, only: [:edit, :update] 
-  resources :rooms, only: [:new, :create]
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
   devise_for :users
   root to: "rooms#index"
 end

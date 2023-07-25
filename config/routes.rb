cccc88873d4730e698eb904e2d8fd1b9b9454ff6
naoki-shiogao <138964908+naoki-shiogao@users.noreply.github.com>
2023-07-25 17:40:50 +0900

    チャットルームの削除機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: [:edit, :update] 
-  resources :rooms, only: [:new, :create] do
+  resources :rooms, only: [:new, :create, :destroy] do
     resources :messages, only: [:index, :create]
   end
   devise_for :users
